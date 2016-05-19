84b35dce5e735f6dc286dd12d00035e4098bedd2
Xiaoxi Guo <xguo@time2work.com>
2016-05-19 10:05:20 +1000

    Changes are made to be compatiable with Azure ML, i.e., lubridate and strplit

---
+++ DataHoursV2_ML.R
@@ -0,0 +1,34 @@
+DataHoursV2_ML <-function(AllData, col, FirstDate, FinishDate.T){
+  ## AllData is a dataframe =(FinishTime=Epoch, Items = items/Tran, )
+  ## But at least AllData needs to include 2 terms, with first being FinishTime (Finish time of each transaction)
+  ## col =  indicates what term to analyse
+  ## FirstDate = character
+  
+  
+  ## Change the Azure ML epoch format to DayTime
+  HourlySale <- aggregate(VendData.stor.temp0$Transactions,
+                          list(hour=cut(as.POSIXct(VendData.stor.temp0$FinishTime, origin = "1970-01-01", tz="GMT"),"hour")),sum)
+  HourlySale.arry <- array(HourlySale$x, dim = nrow(HourlySale), dimnames = list(as.character(HourlySale$hour)))  
+  
+  hrs <- c("00:00:00", "01:00:00", "02:00:00", "03:00:00", "04:00:00", "05:00:00", "06:00:00", 
+           "07:00:00", "08:00:00", "09:00:00", "10:00:00", "11:00:00", "12:00:00", "13:00:00", 
+           "14:00:00", "15:00:00", "16:00:00", "17:00:00", "18:00:00", "19:00:00", "20:00:00", 
+           "21:00:00", "22:00:00", "23:00:00")
+  
+  
+  A1 <- seq(as.Date(FirstDate), as.Date(FinishDate), by="1 day") # all days
+  
+  allhoursName <- paste(rep(A1, each=24), rep(hrs, length(A1)))
+  allhours <- array(data=rep(0, length(allhoursName)), dim=length(allhoursName), dimnames=list(allhoursName)) 
+  
+  for (i in 1:length(allhours)){
+    if (names(allhours[i]) %in% names(HourlySale.arry)){
+      allhours[i] <- HourlySale.arry[which(names(HourlySale.arry)==names(allhours[i]))]
+    }
+  }
+  
+  AllData.hours <- data.frame(Time = as.POSIXlt(names(allhours), "%Y-%m-%d %H:%M:%S", tz="GMT"), Items = unname(allhours))
+  
+  return(AllData.hours)
+  
+}
---
+++ ExceptionalDayandEffectFormatV2_ML.R
@@ -0,0 +1,115 @@
+ExceptionalDayandEffectFormatV2_ML<-function(ExceptionalDatesCSV, FirstDate, FinishDateT){
+  ## ExceptionalDatesCSV = ["ExceptionalDate","Annual","ForecastIgnoreHistory","ForecastDateSpecific","ExceptionalDayTypeID"]
+  ## FirstDate, FinishDate.T = Character
+  ## Output = list(ExceptionalDays, ProximityDays)
+  
+  
+  #########################################################################
+  #####  I: Complete Exceptional Days, from FirstDate to FinishDate.T
+  ##### II: Find proximity days
+  #########################################################################
+
+  
+  #####  I: Complete Exceptional Days, from FirstDate to FinishDate.T
+  
+  #ExceptionalDayandEffects<-ExceptionalDayandEffectFormatV2(ExceptionalDates, FirstDate, FinishDateT)
+  #################
+  FirstYear.temp<- strsplit(FirstDate, split = "-")
+  FirstYear <- FirstYear.temp[[1]]
+  LastYear.temp<- strsplit(FinishDateT, split = "-")
+  LastYear <- LastYear.temp[[1]]
+  
+  AllYears<-seq(from = as.integer(FirstYear[1]), to = as.integer(LastYear[1]), by = 1)
+  
+  ExceptionalDays$ExceptionalDate<- as.Date(ExceptionalDays$ExceptionalDate)
+  ExceptionalDays$Annual <- as.logical((ExceptionalDays$Annual))
+  ExceptionalDays$Annual[is.na(ExceptionalDays$Annual)]<-FALSE
+  print(ExceptionalDays)  
+  
+  
+  ###### Deal with duplicated FALSE and TRUE at Annual: delete FALSE row if same day Annual is TRUE
+  FalseAnnulInd <- which(! ExceptionalDays$Annual)
+  TrueAnnulInd <- which(ExceptionalDays$Annual)
+  print(FalseAnnulInd)
+  print(TrueAnnulInd)
+  
+  if (length(FalseAnnulInd)>0){
+    DeleteInd <-c()
+    for (i in 1 : length(FalseAnnulInd)){
+      print(format(ExceptionalDays$ExceptionalDate[FalseAnnulInd[i]], "%m-%d"))
+      if(format(ExceptionalDays$ExceptionalDate[FalseAnnulInd[i]], "%m-%d") %in% format(ExceptionalDays$ExceptionalDate[TrueAnnulInd], "%m-%d")){
+        DeleteInd<-c(DeleteInd, FalseAnnulInd[i])
+      }
+    }
+  }
+  print(DeleteInd)
+  if (length(DeleteInd)>0){
+    ExceptionalDays <- ExceptionalDays[0-DeleteInd,]
+  }
+  
+  
+  ## Define Unique Index for ExceptionalDayTypeID
+  Annual.Dates<-unique(format(ExceptionalDays$ExceptionalDate[which(ExceptionalDays$Annual)], "%m-%d"))
+  UniqueInd<-setdiff(sample(1:(nrow(ExceptionalDays)+1), nrow(ExceptionalDays), replace=F), 
+                     unique(ExceptionalDays$ExceptionalDayTypeID[!is.na(ExceptionalDays$ExceptionalDayTypeID)]))
+  for (i in 1:length(Annual.Dates)){
+    ExceptionalDays$ExceptionalDayTypeID[which(format(ExceptionalDays$ExceptionalDate, "%m-%d") 
+                                               == Annual.Dates[i])] <- UniqueInd[i]
+  }
+  
+  ExceptionalDays$ExceptionalDayTypeID[is.na(ExceptionalDays$ExceptionalDayTypeID)]<-
+    format(round(runif(length(which(is.na(ExceptionalDays$ExceptionalDayTypeID))), min=0, max=9), 3),nsmall = 4) #fill-in a random number if NA
+  
+  ExceptionalDays2 <- ExceptionalDays[,c(1,2,5)]
+  ExceptionalDays2$ExceptionalDate<- as.Date(ExceptionalDays2$ExceptionalDate)
+  
+  
+  
+  ## Add missing information
+  TrueAnnulInd <- c()
+  TrueAnnulInd <- which(ExceptionalDays$Annual)
+  if (length(TrueAnnulInd)>0){
+    for (i in 1:length(TrueAnnulInd)){
+      ExceptionalDate <-as.Date(paste(as.character(AllYears), "-", as.character(format(ExceptionalDays$ExceptionalDate[TrueAnnulInd[i]], "%m-%d")), sep=""))
+      ExceptionalDayTypeID <- rep(ExceptionalDays$ExceptionalDayTypeID[TrueAnnulInd[i]], length=length(ExceptionalDate))
+      Annual <- rep("TRUE", length(ExceptionalDate))
+      
+      ExceptionalDays2 <- rbind(ExceptionalDays2, data.frame(ExceptionalDate, Annual, ExceptionalDayTypeID))
+      # }
+    }
+    ExceptionalDays2 <- ExceptionalDays2[(! duplicated(ExceptionalDays2$ExceptionalDate)) ,]
+    ExceptionalDays2 <- ExceptionalDays2[order(ExceptionalDays2$ExceptionalDate), ]
+    ExceptionalDays2$Annual <- as.logical(ExceptionalDays2$Annual)
+  }
+  
+  
+  
+  ##### II: Find proximity days
+  ProximityDays <- data.frame(Dates = rep(as.Date("2000-01-01"),length= 2*nrow(ExceptionalDays2)),
+                              Annual = rep(FALSE, length = 2*nrow(ExceptionalDays2)),
+                              ProximityDaysTypeID = rep("???", length = 2*nrow(ExceptionalDays2)))
+  ProximityDays$ProximityDaysTypeID <- as.character(ProximityDays$ProximityDaysTypeID)
+  
+  for (i in 1:nrow(ExceptionalDays2)){
+    if (!(as.character(ExceptionalDays2$ExceptionalDate[i]-1) %in% as.character(ExceptionalDays2$ExceptionalDate))){
+      ProximityDays$Dates[i] <- ExceptionalDays2$ExceptionalDate[i]-1
+      ProximityDays$Annual[i] <- ExceptionalDays2$Annual[i]
+      ProximityDays$ProximityDaysTypeID[i] <- paste(as.character(ExceptionalDays2$ExceptionalDayTypeID[i]), "-", sep="")
+    }
+  }
+  
+  for (i in 1:nrow(ExceptionalDays2)){
+    if ((!(as.character(ExceptionalDays2$ExceptionalDate[i]+1) %in% as.character(ExceptionalDays2$ExceptionalDate)))
+        &&(!(as.character(ExceptionalDays2$ExceptionalDate[i]+1) %in% as.character(ProximityDays$Dates) )) ){
+      ProximityDays$Dates[nrow(ProximityDays)-i+1] <- ExceptionalDays2$ExceptionalDate[i]+1
+      ProximityDays$Annual[nrow(ProximityDays)-i+1] <- ExceptionalDays2$Annual[i]
+      ProximityDays$ProximityDaysTypeID[nrow(ProximityDays)-i+1] <- paste(as.character(ExceptionalDays2$ExceptionalDayTypeID[i]), "+", sep="")
+    }
+  }
+  
+  
+  ProximityDays<-ProximityDays[0-which(ProximityDays$ProximityDaysTypeID == "???"), ]
+  ProximityDays<- ProximityDays[order(ProximityDays$Dates), ]
+  
+  return(list(ExceptionalDays, ProximityDays))
+}
