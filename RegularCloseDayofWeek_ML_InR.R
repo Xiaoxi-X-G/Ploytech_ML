3c547b021072481e183eaa8e295271555148a4f3
Xiaoxi Guo <xguo@time2work.com>
2016-05-20 10:07:08 +1000

    Used to debug in R.studio, as is.na() is not working in Azure ML

---
+++ RegularCloseDayofWeek_ML_InR.R
@@ -0,0 +1,22 @@
+RegularCloseDayofWeek_ML_InR <-function(OpenDayResults){
+  ###2. Find Regular Closing date and exclude from input
+  
+  
+  ###I.2. Find Regular Closing date and exclude from input
+  if (nrow(OpenDayResults)==0){
+    Closedayofweek <-c()
+  }else{
+    
+    RegularDates <- c()
+    RegularHourInd <-which(is.na(OpenDayResults$EffectiveTo))
+    if (length(RegularHourInd)>0){
+      for (i in 1:length(RegularHourInd)){
+        RegularDates<-c(RegularDates, TranslateDayofWeek(OpenDayResults$DayOfWeek[RegularHourInd[i]]))
+      }
+    }
+    Closedayofweek <- setdiff(c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"), unique(RegularDates))
+  }
+  
+  
+  return (Closedayofweek)
+}
